465364078ba1556aeec8a193f6b6ebd90d109009
Ying LUO <ying.luo@johnabbottcollege.net>
2021-03-23 09:48:03 -0400

    new

--- day-1/refresher.js
+++ day-1/refresher.js
@@ -102,6 +102,10 @@ array.find(number=>{
     }
 });
 
+const found = array.find(element => element > 50);
+
+console.log("another way:" + found);
+
 function greaterThan(number) {
     //return all the numbers in array that greater than 50
     //if(number >= 50){
@@ -134,13 +138,17 @@ const students = [{
 
 // Q1: Sort all the students by marks from highest to lowest using sort method
 // ascending:
-students.sort(function(a, b){return a.marks - b.marks});
+const result1 = students.sort(function(a, b){return a.marks - b.marks});
+console.log(result1);
 
 // descending:
-students.sort(function(a, b){return b.marks - a.marks});
+const result2 = students.sort(function(a, b){return b.marks - a.marks});
+console.log(result2);
 
 // Q2: Filter and display all the students with marks greater than 80 using filter method
-students.filter(a => a.marks > 80);
+const result3 = students.filter(a => a.marks > 80);
+console.log(result3);
 
 // q3: Create a new array from the array above in which the marks of all students is 5 more than current marks using map
-students.map(a => a.marks + 5);
+const result4 = students.map(a => a.marks + 5);
+console.log(result4);
