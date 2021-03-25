a42211bde12183d4975f53d431a6b6988134ca86
Ying LUO <ying.luo@johnabbottcollege.net>
2021-03-25 11:18:40 -0400

    db connection

--- day-2/myapp/app.js
+++ day-2/myapp/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ordersRouter = require('./routes/orders');
+var superherosRouter = require('./routes/superheros');
 
 var app = express();
 
@@ -18,5 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/orders', ordersRouter);
+app.use('/superheros', superherosRouter);
 
 module.exports = app;
---
+++ day-2/myapp/db.js
@@ -0,0 +1,22 @@
+'user strict';
+
+var mysql = require('mysql');
+
+//local mysql db connection
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'StrongPWD123',
+    database:'superherodb'
+});
+
+connection.connect(function(err){
+    if(err) {
+        console.log("DB connection failed");
+        throw err;
+    }else{
+        console.log("DB connection successful");
+    }
+});
+
+module.exports = connection;
